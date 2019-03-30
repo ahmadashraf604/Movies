df1210daf2301746057904eefe34751633770f8a
ashraf <ashraf@Abanoubs-Mac.local>
2019-03-30 21:17:26 +0200

    add alamo fire

--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import Alamofire
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let strURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity&api_key=e07070a3952fcf18a91b6d1aaf196f8c"
+        
+        Alamofire.request(strURL).responseJSON { (responseObject) -> Void in
+            
+            print(responseObject)
+            
+            if responseObject.result.isSuccess {
+//                let resJson = JSON(responseObject.result.value!)
+//                success(resJson)
+                print(responseObject)
+            }
+//            if responseObject.result.isFailure {
+//                let error : NSError = responseObject.result.error!
+//                failure(error)
+//            }
+            
+            
+        }
     }
 
 
